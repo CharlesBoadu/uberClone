4ed088ccb086b10916e35524a496ca17061088d2
Charles Osei Boadu <charles.boadu@stu.ucc.edu.gh>
2023-02-26 22:53:29 +0000

    Adding files

---
+++ screens/MapScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const mapScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>mapScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default mapScreen
+
+const styles = StyleSheet.create({})
